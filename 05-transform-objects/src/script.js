c13e26f1eab65eaa7e39ea19f8ae2993c8eeee7a
JaMussCraft <jamesnaruto04@gmail.com>
2023-07-17 19:08:50 -0400

    Finished 05

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,32 +9,63 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(1,1,-20)
-scene.add(mesh)
+const cubes = new THREE.Group()
+scene.add(cubes)
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(50)
+scene.add(axesHelper)
+
+// Lights
+const light = new THREE.PointLight()
+scene.add(light)
+
+
+// Group
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 'red' })
+)
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 'cyan' })
+)
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 'green' })
+)
+
+cube1.position.set(1,0,0)
+cube2.position.set(0,1,0)
+cube3.position.set(0,0,1)
+
+cubes.add(cube1)
+cubes.add(cube2)
+cubes.add(cube3)
+
+cubes.position.set(0,0,5)
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+  width: 800,
+  height: 600,
 }
 
 /**
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
 scene.add(camera)
+camera.position.set(2, 2, 10)
+// camera.lookAt(mesh.position)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
-ren
+renderer.render(scene, camera)
