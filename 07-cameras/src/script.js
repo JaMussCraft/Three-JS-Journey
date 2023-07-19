47f136cbece2e43450ecdb170bbcb3e94734a673
James Wong <99109398+JaMussCraft@users.noreply.github.com>
2023-07-19 18:07:20 -0400

    Completed 07

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
 /**
@@ -23,7 +24,6 @@ const cursor = {
 addEventListener('mousemove', (e) => {
     cursor.x = e.clientX / sizes.width - 0.5
     cursor.y = e.clientY / sizes.height - 0.5
-    console.log(cursor.x)
 })
 
 // Scene
@@ -45,6 +45,10 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+// Orbit Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -59,8 +63,14 @@ const tick = () => {
 
   // Update objects
   // mesh.rotation.y = elapsedTime;
-  mesh.position.x += cursor.x
-  mesh.position.y += cursor.y
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+  // camera.position.y = -cursor.y * Math.PI * 2
+  // camera.lookAt(mesh.position)
+
+  // Update Controls
+  controls.update()
+  console.log(controls)
 
   // Render
   renderer.render(scene, camera)
