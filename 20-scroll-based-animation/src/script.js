6729127c5804c51b7d9e6a6f61f8ac6bd74c21e1
James Wong <99109398+JaMussCraft@users.noreply.github.com>
2023-07-26 13:05:35 -0400

    Finished 20 - Camera section

--- 20-scroll-based-animation/src/script.js
+++ 20-scroll-based-animation/src/script.js
@@ -38,7 +38,7 @@ gradientT.magFilter = THREE.NearestFilter
  */
 const objectDistance = 4
 
-const material = new THREE.MeshToonMaterial({ color: parameters.color, gradientMap: gradientT })
+const material = new THREE.MeshToonMaterial({ gradientMap: gradientT })
 const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material)
 const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material)
 const mesh3 = new THREE.Mesh(new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16), material)
@@ -48,6 +48,10 @@ const sectionMeshes = [mesh1, mesh2, mesh3]
 mesh1.position.y -= objectDistance * 0
 mesh2.position.y -= objectDistance * 1
 mesh3.position.y -= objectDistance * 2
+
+mesh1.position.x = 2
+mesh2.position.x = -2
+mesh3.position.x = 2
 
 // mesh1.position.x = -1
 // mesh2.position.x = 0
@@ -87,10 +91,16 @@ window.addEventListener('resize', () => {
 /**
  * Camera
  */
+
+// Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -109,8 +119,30 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
+let scrollY = window.scrollY
+
+const cursor = {
+  x: 0,
+  y: 0,
+}
+
+addEventListener('mousemove', (e) => {
+  // normalize the values
+  cursor.x = e.clientX / sizes.width - 0.5
+  cursor.y = e.clientY / sizes.height - 0.5
+
+})
+
+addEventListener('scroll', () => {
+  scrollY = window.scrollY
+})
+
+let previousTime = 0
+
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
 
   // Animate Meshes
   for (const mesh of sectionMeshes) {
@@ -118,11 +150,19 @@ const tick = () => {
     mesh.rotation.y = elapsedTime * 0.2
   }
 
+  // Animate Camera
+  cameraGroup.position.y = -scrollY / sizes.height * objectDistance
+
+  camera.position.x += ((cursor.x * 0.5) - camera.position.x) * deltaTime * 1.5
+  camera.position.y += ((-cursor.y * 0.5) - camera.position.y) * deltaTime * 1.5
+
   // Render
   renderer.render(scene, camera)
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick)
 }
+
+console.log(document.body.scrollHeight)
 
 tick()
